ba7c7cba1a55da517f1d243c5deba8e70267f824
Ishwar Chandra Prakash <ishwar@Ishwars-MacBook-Air.local>
2024-06-22 17:01:49 +0530

    Variables, Constants And Data Types

---
+++ Programming Using C++ /Lecture_4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    //Declare two variables and then assign them values 3,5 and add them 
+    
+    int a = 3;
+    int b = 5;
+
+    cout<<"a+b"<<endl<<a+b;
+
+    return 0;
+}
