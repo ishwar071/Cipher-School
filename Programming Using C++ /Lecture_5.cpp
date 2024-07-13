4dee545021173b693164ff3fefbd34e4df83fa85
Ishwar Chandra Prakash <ishwar@Ishwars-MacBook-Air.local>
2024-07-14 00:08:39 +0530

    shy

---
+++ Programming Using C++ /Lecture_5.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a,b;
+    int c,d;
+    cout<<"Input 4 integer a,b,c and d"<<endl;
+    
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+    
+    //Statement : a>b
+
+    cout<<(a>b && c>d);
+    
+    // c++ interprets true as 1
+    // c++ interprets false as 0
+
+    return 0;
+
+}
