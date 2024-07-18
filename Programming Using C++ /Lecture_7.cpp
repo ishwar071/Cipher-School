60b5200a76fae2398152a8a885fc1a7ed8751976
Ishwar Chandra Prakash <ishwar@Ishwars-MacBook-Air.local>
2024-07-18 16:39:21 +0530

    Use of an Arrays

---
+++ Programming Using C++ /Lecture_7.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;      // variable name x of type int --> It can store single integer which means x can store 1 or 2 or any single integer .
+
+    // Arrays can be used to store multiple values in a single variable name  
+
+    int a[5];   // This means it is an array of name a which can store multiple integer , it can store 6 integer
+                // Note: The indexing of array starts from 0 not from 1. 
+                
+        // Elements starting from 0 to 5. 
+
+        // The first element will have the an index 0 --> a[0]
+        // The second element will have the an index 1 --> a[1]
+
+        //And goes till a[5]
+
+
+
+}
