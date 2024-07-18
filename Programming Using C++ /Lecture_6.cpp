db588ae9d31c7ad82e28b1e3609b3bad4bfd73e0
Ishwar Chandra Prakash <ishwar@Ishwars-MacBook-Air.local>
2024-07-18 16:04:15 +0530

    Conditional Statements, If Else

--- Programming Using C++ /Lecture_6.cpp
+++ Programming Using C++ /Lecture_6.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 int main()
 {
-    cout<<"hello if else";
+    int Marks ;
+    cout<<"Dear Student, Please enter the marks to calculate the grade : ";
+    cin>>Marks;
+    
+    if(Marks > 90 ){
+        cout<<"A";
+    }
+    else if (Marks > 80){
+        cout<<"B";
+    }
+    else if (Marks > 70){
+        cout<<"C";
+    }
+    else{
+        cout<<"Pass";
+    }
+
+return 0;
 
 }
